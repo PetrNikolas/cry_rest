0cc32e4e15628e91c7f1599b93cf0f92137c071c
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2018-10-09 20:24:11 +0200

    added a Auth

--- config/routes.cr
+++ config/routes.cr
@@ -10,6 +10,7 @@ Amber::Server.configure do
     plug Amber::Pipe::Session.new
     plug Amber::Pipe::Flash.new
     plug Amber::Pipe::CSRF.new
+    plug Authenticate.new
   end
 
   pipeline :api do
@@ -28,7 +29,20 @@ Amber::Server.configure do
   end
 
   routes :web do
+    # Auth
+    get "/profile", UserController, :show
+    get "/profile/edit", UserController, :edit
+    patch "/profile", UserController, :update
+    get "/signin", SessionController, :new
+    post "/session", SessionController, :create
+    get "/signout", SessionController, :delete
+    get "/signup", RegistrationController, :new
+    post "/registration", RegistrationController, :create
+
+    # Posts
     resources "/posts", PostController
+
+    # Homepage
     get "/", HomeController, :index
   end
 
